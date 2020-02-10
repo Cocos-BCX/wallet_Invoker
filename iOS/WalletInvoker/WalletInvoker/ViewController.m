4308551ccdf4b351fce198d7e5493c6804de34e1
邵银岭 <534585931@qq.com>
2020-02-10 17:35:34 +0800

    fix call wallet tool bug

--- iOS/WalletInvoker/WalletInvoker/ViewController.m
+++ iOS/WalletInvoker/WalletInvoker/ViewController.m
@@ -22,7 +22,7 @@
 
 - (IBAction)loginBtnClick:(UIButton *)sender {
     CocosLoginObj *loginObj = [[CocosLoginObj alloc] init];
-    loginObj.dappIcon = @"https://gz.bcebos.com/v1/tokenpocket/temp/mobile_sdk_demo.png";
+    loginObj.dappIcon = @"https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1552892908,1688888225&fm=26&gp=0.jpg";
     loginObj.dappName = @"Dapp Test";
     loginObj.desc = @"请求登录的描述信息";
     [CocosWalletApi sendObj:loginObj];
@@ -31,7 +31,7 @@
 - (IBAction)transferBtnClick:(UIButton *)sender {
     CocosTransferObj *transfer = [[CocosTransferObj alloc] init];
     transfer.dappName = @"Dapp Test";
-    transfer.dappIcon = @"https://gz.bcebos.com/v1/tokenpocket/temp/mobile_sdk_demo.png";
+    transfer.dappIcon = @"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1581336823814&di=854974e1a9bf2f774add305a7c0d417c&imgtype=0&src=http%3A%2F%2Fimg.jk51.com%2Fimg_jk51%2F78884959.jpeg";
     transfer.desc = @"请求转账的描述信息";
     transfer.symbol = @"COCOS";
     transfer.to = @"syling1";
@@ -45,11 +45,12 @@
     
     CocosCallContractObj *callContract = [[CocosCallContractObj alloc] init];
     callContract.dappName = @"myDapp";
-    callContract.dappIcon = @"https://gz.bcebos.com/v1/tokenpocket/temp/mobile_sdk_demo.png";
-    callContract.param = @[@"COCOS"];
+    callContract.dappIcon = @"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1581336466306&di=86e0f6afdd435ee39dc57fbf7de11d81&imgtype=0&src=http%3A%2F%2F07.imgmini.eastday.com%2Fmobile%2F20180306%2F20180306_1f79b02260b76a95a2a8dfe68ef9fe10.png";
+    callContract.param = @[@(10)];
     callContract.from = @"syling";
-    callContract.contract = @"contract.dicegame";
+    callContract.contract = @"contract.testnumber";
     callContract.method = @"hello";
+    callContract.desc = @"请求调用合约的描述信息";
     [CocosWalletApi sendObj:callContract];
 }
 
